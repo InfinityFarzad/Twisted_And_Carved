55b662a3579cc1e55c01953ff56c6b0e1bc5057c
InfinityFarzad <10farzad.karami@gmail.com>
2026-07-22 23:15:31 +0330

    i hate world gen #47

--- src/main/java/net/farzad/twisted_and_carved/common/config/TwistedAndCarvedConfigScreen.java
+++ src/main/java/net/farzad/twisted_and_carved/common/config/TwistedAndCarvedConfigScreen.java
@@ -5,7 +5,6 @@ import eu.midnightdust.lib.config.MidnightConfig;
 public class TwistedAndCarvedConfigScreen extends MidnightConfig {
     public static final String SERVER = "server";
 
-    @Comment(category = SERVER) public static String woah;
-    @Server @Entry(category = SERVER) public static float dmg = 5;
+    @Server @Entry(category = SERVER) public static float falchion_damage = 3;
 
 }
